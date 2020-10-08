2ed1c9fc0585d27bcd2d98f90b3af26b8366d416
Jan Robert <janmartinrobert@gmail.com>
2020-10-08 22:59:40 +0200

    Add static example todos

--- frontend/src/context/todo/TodoContext.js
+++ frontend/src/context/todo/TodoContext.js
@@ -1,16 +1,19 @@
 import React from "react";
-import {todoContextReducer} from "./todo-reducer";
+import { todoContextReducer } from "./todo-reducer";
+import exampleTodos from "./example-todos.json";
 
 export const TodoStateContext = React.createContext(undefined);
 export const TodoDispatchContext = React.createContext(undefined);
 
-export const TodoContextProvider = ({children}) => {
-    const [state, dispatch] = React.useReducer(todoContextReducer, {
-        todos: []
-    });
-    return <TodoStateContext.Provider value={state}>
-        <TodoDispatchContext.Provider value={dispatch}>
-            {children}
-        </TodoDispatchContext.Provider>
+export const TodoContextProvider = ({ children }) => {
+  const [state, dispatch] = React.useReducer(todoContextReducer, {
+    todos: exampleTodos,
+  });
+  return (
+    <TodoStateContext.Provider value={state}>
+      <TodoDispatchContext.Provider value={dispatch}>
+        {children}
+      </TodoDispatchContext.Provider>
     </TodoStateContext.Provider>
+  );
 };
