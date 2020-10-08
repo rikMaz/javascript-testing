5b0795730d080b8c43294b56530baf37c788153e
Jan Robert <janmartinrobert@gmail.com>
2020-10-08 23:02:21 +0200

    Change request for add todo from put to post

--- frontend/src/service/todo-service.js
+++ frontend/src/service/todo-service.js
@@ -1,22 +1,21 @@
-const axios = require('axios');
+const axios = require("axios");
 
 export const getTodos = async () => {
-    return await axios.get('/api/todo');
+  return await axios.get("/api/todo");
 };
 
 export const addTodo = async (description) => {
-    return await axios.put('/api/todo', {
-        description: description
-    });
+  return await axios.post("/api/todo", {
+    description: description,
+  });
 };
 
 export const deleteTodo = async (id) => {
-    return await axios.delete(`/api/todo/${id}`);
+  return await axios.delete(`/api/todo/${id}`);
 };
 
 export const setTodoStatus = async (id, status) => {
-    return await axios.put(`/api/todo/${id}/status`, {
-        status: status
-    });
+  return await axios.put(`/api/todo/${id}/status`, {
+    status: status,
+  });
 };
-
