a6bbe94a889d1f63e7e3a85d317141d0c12ad066
DennisMaas <mail@dennismaas.de>
2020-10-09 09:58:35 +0200

    init commit empty project. add spring web and lombok dependencies. add backend folder

---
+++ backend/src/test/java/de/neuefische/todowebapp/TodoWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.todowebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
